387a9740244acd35373c98a426a9ecca0d2e1163
Nathen Bowes <nathen.bowes@shopify.com>
2020-03-28 13:49:44 -0700

    add useContext + custom useAppContext hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from '../state/reducer'
+import Context from '../context'
 import PublishMessage from './PublishMessage'
 import MessageBoard from './MessageBoard'
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  console.log('state', state)
   return (
-    <div >
+    <Context.Provider value={{ state, dispatch}}>
       <h2>Reaction</h2>
       <hr />
-      <PublishMessage dispatch={dispatch} />
-      <MessageBoard messages={state.messages}/>
-    </div>
+      <PublishMessage />
+      <MessageBoard />
+    </Context.Provider>
   );
 }
 
