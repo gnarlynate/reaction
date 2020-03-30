288ba4ff73621804ceda1467578fbb2b93f06a97
Nathen Bowes <nathen.bowes@shopify.com>
2020-03-30 16:25:04 -0700

    complete setup for PubNub for multiple users

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import reducer, { initialState } from '../state/reducer'
 import Context from '../context'
+import PubSub from '../pubsub'
 import PublishMessage from './PublishMessage'
 import MessageBoard from './MessageBoard'
-import '../pubsub'
+
+const pubsub = new PubSub()
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  useEffect(()=>{
+    pubsub.addListener({
+      message: messageObject => {
+        const { channel, message } = messageObject
+    
+        console.log('Recieved message', message, 'channel', channel)
+        dispatch(message)
+      }
+    })
+  }, [])
+
   return (
-    <Context.Provider value={{ state, dispatch}}>
+    <Context.Provider value={{ state, dispatch, pubsub}}>
       <h2>Reaction</h2>
       <hr />
       <PublishMessage />
