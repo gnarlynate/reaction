d3100657865d201f27d939909e191a98375ce262
Nathen Bowes <nathen.bowes@shopify.com>
2020-03-30 18:57:30 -0700

    add message reactions to MessageReactions

---
+++ src/components/MessageReactions.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function MessageReactions({messageReactions}){
+  console.log('message reactions:', messageReactions)
+  if(!messageReactions) return null
+
+  return (
+    messageReactions.map((reaction, index) => {
+      const { id, emoji, username } = reaction
+
+      return(
+        <span key={id}>
+          <em>{username}:</em>
+          {emoji}
+          {index !== messageReactions.length-1 ? ', ' : null}
+        </span>
+      )
+    })
+  )
+}
+
+export default MessageReactions
