05bf8375c83082208a788f23226442c7eddf444c
Nathen Bowes <nathen.bowes@shopify.com>
2020-03-30 17:27:28 -0700

    start reaction logic in actions + reducer + types + MessageBoard

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,8 +1,28 @@
-import { NEW_MESSAGE, SET_USERNAME } from './types'
+import { NEW_MESSAGE, SET_USERNAME, REACTION_OBJECTS } from './types'
 
-export const initialState = { messages: [], username: "anonymous"}
+export const initialState = { 
+  messages: [], 
+  username: "anonymous",
+  reactionsMap: {}
+}
+
+const REACTION_TYPES = REACTION_OBJECTS.map(reactionObject => reactionObject.type)
 
 const reducer = (state, action) => {
+  if(REACTION_TYPES.includes(action.type)){
+    let reactionsMap;
+
+    const { messageId } = action.item
+
+    const messageReactions = state.reactionsMap[messageId] || []
+
+    reactionsMap = {
+      ...state.reactionMap,
+      [messageId]: [...messageReactions, action.item]
+    }
+    return { ...state, reactionsMap}
+  }
+
   switch(action.type) {
     case NEW_MESSAGE:
       return {...state, messages:[...state.messages, action.item]}
