28cba0b750cce84542f428eafe1c2b501a2484a5
Nathen Bowes <nathen.bowes@shopify.com>
2020-03-30 17:00:49 -0700

    complete PubNub implementation

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -8,12 +8,13 @@ function MessageBoard() {
     <div>
       {
         messages.map(message => {
-          const {id, text, timestamp} = message
+          const {id, text, username, timestamp} = message
 
           return (
             <div key={id}>
               <h4>{new Date(timestamp).toLocaleString()}</h4>
               <p>{text}</p>
+              <h4>- {username}</h4>
             </div>
           )
         })
